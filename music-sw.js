618848f0095e5dfc2ed15c8d2834422cec17b591
kameusagiyahoo <nywhatisbudou@yahoo.co.jp>
2026-08-29 08:22:51 +0900

    Prune stale audio versions in Service Worker cache

--- music-sw.js
+++ music-sw.js
@@ -32,6 +32,20 @@ self.addEventListener("fetch", (event) => {
 
     const response = await fetch(request);
     if (response.ok) {
+      const version = url.searchParams.get("gmv");
+      if (version) {
+        const keys = await cache.keys();
+        await Promise.all(keys.map(async (cachedRequest) => {
+          const cachedUrl = new URL(cachedRequest.url);
+          if (
+            cachedUrl.pathname === url.pathname &&
+            cachedUrl.searchParams.get("gmv") &&
+            cachedUrl.searchParams.get("gmv") !== version
+          ) {
+            await cache.delete(cachedRequest);
+          }
+        }));
+      }
       await cache.put(request, response.clone());
     }
     return response;
